aa4ce51f671898dc896de20beba4d7097e60a7ab
kungfumastah <pi@rpi-01.(none)>
2016-04-19 13:57:07 -0500

    Removed Celsius

--- temperature/read_temp_cron.py
+++ temperature/read_temp_cron.py
@@ -34,7 +34,7 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         temp_f = temp_c * 9.0 / 5.0 + 32.0
-        return temp_c, temp_f
+        return temp_f
 
 with open(log_file, "a") as myfile:
         myfile.write(time.strftime("%c") + "\t" + str(read_temp()) + '\n')
